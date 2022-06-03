c6d46bad76bbb4ecb2049b66870f2bd54faf7fb4
Iano-theDev <sinceianmike@gmail.com>
2022-06-03 08:10:17 +0300

    complete 8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interprated, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + ' ' + str[114:118] + ' ' + str[0:6]
+print(str)
