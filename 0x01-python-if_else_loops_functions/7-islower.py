8927b0600a6a7fb32b1981d15102b882a5cdc6a4
Iano-theDev <sinceianmike@gmail.com>
2022-06-06 20:48:41 +0300

    Fix output err, made executable

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in range(ord('a'), ord('z') + 1):
-        print("{:c} is lower".format(c))
+    for c in range(ord('a'), ord('z') + 1)
         return(True)
     else:
-        print("{:c} is upper".format(c))
         return(False)
