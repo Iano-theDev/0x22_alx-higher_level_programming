7e49ea74b62f4f7b7eba3e80db6632d26c7759bb
Iano-theDev <sinceianmike@gmail.com>
2022-06-06 21:13:40 +0300

    Hot fix function

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in range(ord('a'), ord('z') + 1):
-        return (True)
-    else:
-        return (False)
+    return(ord('a') <= ord(c) <= ord('z'))
