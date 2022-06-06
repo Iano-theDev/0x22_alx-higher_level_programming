bdd90fa391101aaa71308e90c7c61c89c2975bfb
Iano-theDev <sinceianmike@gmail.com>
2022-06-06 21:01:08 +0300

    Fix missing : error

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in range(ord('a'), ord('z') + 1)
-        return(True)
+    for c in range(ord('a'), ord('z') + 1):
+        return True
     else:
-        return(False)
+        return False
