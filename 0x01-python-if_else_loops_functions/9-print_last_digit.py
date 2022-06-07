1a1d25b818eac30226b99ba153d7b5894f893692
Iano-theDev <sinceianmike@gmail.com>
2022-06-07 11:12:44 +0300

    Fix output error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return(number % -10)
+        return(print("{:02d}".format(number % -10)))
     else:
-        return(number % 10)
+        return(print("{:02d}".format(number % 10)))
