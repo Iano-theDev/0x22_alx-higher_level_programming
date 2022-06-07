925c00002164a9c72cba0a03f86b0a0245999694
Iano-theDev <sinceianmike@gmail.com>
2022-06-07 23:19:02 +0300

    Correct output error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz ", end='')
         elif num % 5 == 0:
             print("Buzz ", end='')
-        elif (num % 3 == 0) and (num % 5 == 0):
+        elif num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end='')
         else:
             print("{:d} ".format(num), end='')
