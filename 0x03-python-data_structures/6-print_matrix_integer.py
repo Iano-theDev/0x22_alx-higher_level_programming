b1a34387842497327b5af1ff7786ac184a1b7a6d
Iano-theDev <sinceianmike@gmail.com>
2022-06-16 15:41:51 +0300

    Create matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for elm in matrix:
-        print(" ".join("{:d}".format(i) for i in elm)
+    if not matrix:
+        return
+    text = ""
+    for line in matrix:
+        if not line:
+            text += "\n"
+            continue
+        text += "{:d}".format(line[0])
+        for i in range(1, len(line)):
+            text += " {:d}".format(line[i])
+        text += "\n"
+    print(text, end="")
