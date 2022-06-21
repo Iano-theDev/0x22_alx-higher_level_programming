94bc7bc8b036b4bccdb9cb9202d87e93577683b6
Iano-theDev <sinceianmike@gmail.com>
2022-06-21 04:35:38 +0300

    Remove whitespace

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     item = 0
-    for item in range (x):
+    for item in range(x):
         try:
             print(my_list[item], end='')
             item += 1
