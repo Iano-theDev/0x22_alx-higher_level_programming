5abb43e62edf1e2653ae3e2da73e86f391aa85e2
Iano-theDev <sinceianmike@gmail.com>
2022-06-23 20:20:45 +0300

    Safe division using exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    n = 0
+    new_list = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            n += 1
+            new_list += 1
         except (ValueError, TypeError):
             continue
     print()
-    return n
+    return new_list
