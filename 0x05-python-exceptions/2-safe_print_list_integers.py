0af8d528edb947f75562a703da102b063a4a19a4
Iano-theDev <sinceianmike@gmail.com>
2022-06-23 19:43:07 +0300

    Fix variable error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    n = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-            i += 1
+            print("{:d}".format(my_list[i]), end="")
+            n += 1
         except (ValueError, TypeError):
             continue
     print()
-    return i
+    return n
