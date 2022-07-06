bccdd34aac4cf9df762538fea5d39b687dcaf50d
Iano-theDev <sinceianmike@gmail.com>
2022-07-06 21:43:50 +0300

    Correct documentation error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+'''
+adds all arguments to a Python list, then saves them to a file
+'''
 
 from sys import argv
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
 
 filename = "add_item.json"
 
