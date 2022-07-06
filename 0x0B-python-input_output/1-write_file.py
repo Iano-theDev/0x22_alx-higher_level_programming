4972a75a5cd73ef779f14c9c7af385789214d438
Iano-theDev <sinceianmike@gmail.com>
2022-07-06 17:46:39 +0300

    CPycodestyle correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,10 @@ Module for write_file function
 
 
 def write_file(filename="", text=""):
-    '''Writes a string to a file (UTF8) and returns number of characters written'''
+    '''Writes a string to a file (UTF8) and returns
+
+     number of characters written
+     '''
     with open(filename, 'r+', encoding="utf-8") as f:
         f.write(text)
         return (len.text)
-        
