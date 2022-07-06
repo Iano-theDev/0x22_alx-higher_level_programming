100549de94ef06ec793133d7e79e12caa3828c74
Iano-theDev <sinceianmike@gmail.com>
2022-07-06 17:09:16 +0300

    Removed blank line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,6 @@ Contains the read_file function
 
 def read_file(filename=""):
     '''reads a text file in utf-8 encoding and prints to stdout'''
-
     with open(filename,'r', encoding="utf-8") as f:
         for line in f:
             print(line, end='')
