2fb391f4a7910cbae34eeb35b0ed233d29c88242
Iano-theDev <sinceianmike@gmail.com>
2022-07-06 17:50:58 +0300

    Changed mode to write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
 
      number of characters written
      '''
-    with open(filename, 'r+', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
