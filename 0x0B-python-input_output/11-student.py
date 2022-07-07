a2301bb3f9db59d0909672ecba63b1889d05cf15
Iano-theDev <sinceianmike@gmail.com>
2022-07-07 09:51:21 +0300

    load and reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+contains Student class
+'''
+
+
+def __init__(self, fistname, last_name, age):
+    '''initialize the student'''
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+
+def to_json(self, attrs=None):
+    '''
+    retuens a dictionary representation of a student instance
+    with specified attributes
+    '''
+    if attrs is None:
+        return self.__dict__
+    new_dict = {}
+    for a in attrs:
+        try:
+            new_dict[a] = self.__dict__[a]
+        except Exception:
+            pass
+    return new_dict
+
+
+def reload_from_json(self, json):
+    '''replaces all attributes of the student instance'''
+    for key in json:
+        try:
+            setattr(self, key, json[key])
+        except Exception:
+            pass
