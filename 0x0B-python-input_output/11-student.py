ab72f24085707e486e15a77d8a687fdc0d1087ce
Iano-theDev <sinceianmike@gmail.com>
2022-07-07 09:57:56 +0300

    Add whitespace around operator

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -21,7 +21,7 @@ def to_json(self, attrs=None):
     new_dict = {}
     for a in attrs:
         try:
-            new_dict[a] =self.__dict__[a]
+            new_dict[a] = self.__dict__[a]
         except Exception:
             pass
     return new_dict
