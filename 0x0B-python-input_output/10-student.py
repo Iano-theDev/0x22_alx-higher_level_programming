c910cf61f93b7fec67ac38d10282e54c55d5c094
Iano-theDev <sinceianmike@gmail.com>
2022-07-07 07:13:45 +0300

    stdent attributes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+contains class student
+'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        '''initializes the student class'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        returns a dictionary representation of a student
+        instance with specified attributes
+        '''
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
