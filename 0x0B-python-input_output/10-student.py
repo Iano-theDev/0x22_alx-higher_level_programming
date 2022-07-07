504a79101a70ae26b7860ecefc88d87e7facc88d
Iano-theDev <sinceianmike@gmail.com>
2022-07-07 08:02:32 +0300

    Exception correction

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -22,6 +22,6 @@ class Student:
         for a in attrs:
             try:
                 new_dict[a] = self.__dict__[a]
-            except ValueError:
+            except Exception:
                 pass
         return new_dict
