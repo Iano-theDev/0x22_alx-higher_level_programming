6bf7a51937f6b61fc392cc3cd1e47cff1de0bedc
Iano-theDev <sinceianmike@gmail.com>
2022-07-16 09:09:30 +0300

    text_indentation module and test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Contains the text_indentation module"""
+
+
+def text_indentation(text):
+    """separates text by two new lines after "?", ":", "." """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
