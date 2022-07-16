6dba9a20087afe5d3568de9397358d526a4f6150
Iano-theDev <sinceianmike@gmail.com>
2022-07-16 08:16:27 +0300

    Fix new line error

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,8 +21,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for i in line:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) \
+                    of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
