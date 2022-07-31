3d9ae3857c65b9726f3d49db8d1c1e214c89d551
Iano-theDev <sinceianmike@gmail.com>
2022-07-31 22:10:45 +0300

    Fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         """returns a printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += '\n'.join(Rectangle.print_symbol * self.__width
+            string += '\n'.join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
