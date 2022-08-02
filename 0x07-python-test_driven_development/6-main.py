a020df3bf1aa92f11ac7ba3566620998aacae60d
Iano-theDev <sinceianmike@gmail.com>
2022-08-02 19:16:59 +0300

    add tests

--- 0x07-python-test_driven_development/6-main.py
+++ 0x07-python-test_driven_development/6-main.py
@@ -2,4 +2,4 @@
 max_integer = __import__('6-max_integer').max_integer
 
 print(max_integer([1, 2, 3, 4]))
-print(max_integer([1, 3, 4, 2]))
+print(max_integer([1, "ian", 2]))
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -19,11 +19,40 @@ class TestMaxIteger(unittest.TestCase):
     def test_max_integer(self):
         """test for list passed is an empty list"""
         self.assertEqual(max_integer([]), None)
-        
     
-
-
-
+    def test_max_at_end(self):
+        """Testing for max integer at the end of the list"""
+        n = [1, 34, 43, 3, 59]
+        self.assertEqual(max_integer(n), 59)
+    
+    def test_max_at_beginning(self):
+        """Testing for max integer at the beinning of the list"""
+        n = [59, 1, 34, 43, 3]
+        self.assertEqual(max_integer(n), 59)
+        
+    def test_max_at_middle(self):
+        """Testing for max integer at the middle of the list"""
+        n = [1, 34, 59, 32, 12]
+        self.assertEqual(max_integer(n), 59)
+    
+    def test_one_negative(self):
+        """Test for one negative number in the list"""
+        n = [1, 32, 4, 14, -6]
+        self.assertEqual(max_integer(n), 32)      
+    
+    def test_all_negative(self):
+        """Testing for list with all negative numbers"""
+        n = [-1, -34, -43, -3, -59]
+        self.assertEqual(max_integer(n), -1)
+        
+    def test_single_item(self):
+        """Test for only one list item"""
+        self.assertEqual(max_integer([5]), 5)
+    
+    def test_non_int(self):
+        """Testing for a non integer type list"""
+        n = [1, 2, "Ian", 3, 7]
+        self.assertRaises(TypeError)
 
 
 
