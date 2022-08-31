71ce713ad381343c62b2cdd73466a9536d208f79
Iano-theDev <sinceianmike@gmail.com>
2022-08-31 11:53:41 +0300

    fix pycodestyle error

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """BaseGeometry Class"""
     def area(self):
         """raises an exception"""
-        raise Exception ("area() is not implemented")
+        raise Exception("area() is not implemented")
