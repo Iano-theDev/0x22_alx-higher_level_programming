2607eedf15b4a7d3ac92f9f92ecf49f61b3f6b2a
Iano-theDev <sinceianmike@gmail.com>
2022-08-31 10:15:30 +0300

    fix pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Contains class Mylist"""
 
+
 class MyList(list):
     """subclass of list"""
+
     def __init__(self):
         """Initialize object"""
         super().__init__()
-    
+
     def print_sorted(self):
         """prints the sorted list"""
-        print(sorted(self)) 
+        print(sorted(self))
