71d6025ce88e525e15b9b8c13f9f1d8fe6489639
Iano-theDev <sinceianmike@gmail.com>
2022-08-31 10:27:25 +0300

    Check if objects are of the same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to check if objects are of same class"""
+
+def is_same_class(obj, a_class):
+    """Returns True if object is exactly an instance of the specifed class
+    Otherwise, returns False
+    """
+    return (type(obj) == a_class)
