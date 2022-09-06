77b18cdedb02fa2a54b072bec3d2a4a9576d2eef
Iano-theDev <sinceianmike@gmail.com>
2022-09-06 19:53:38 +0300

    fix typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArguments = process.argv.length;
 if (myArguments === 2) {
-  console.log('no argument');
+  console.log('No argument');
 } else if (myArguments === 3) {
   console.log('Argument found');
 } else {
