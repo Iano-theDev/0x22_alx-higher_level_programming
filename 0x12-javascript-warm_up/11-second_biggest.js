f8a96b7bb3b5730827b744318695f5289b43f8a3
Iano-theDev <sinceianmike@gmail.com>
2022-09-08 19:27:39 +0300

    update sort function in script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,9 @@
 const myInts = process.argv.slice(2);
 let secondBiggest = 0;
 if (myInts.length > 1) {
-  myInts.sort();
+  myInts.sort(function (a, b) {
+    return a - b;
+  });
   secondBiggest = myInts.slice(-2, -1);
 }
 console.log(parseInt(secondBiggest));
