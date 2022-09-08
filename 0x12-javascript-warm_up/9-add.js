bc7c473c33ddb0295eb946529abcf5f1e65aec37
Iano-theDev <sinceianmike@gmail.com>
2022-09-08 18:28:36 +0300

    Update task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const a = process.argv[2];
-const b = process.argv[3];
+const firstInt = process.argv[2];
+const secondInt = process.argv[3];
 function add (a, b) {
-  console.log(parseInt(a) + parseInt(b));
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  } else {
+    return (parseInt(a) + parseInt(b));
+  }
 }
-add(a, b);
+console.log(add(firstInt, secondInt));
