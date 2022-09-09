be204be94d1ea7c645dfbfc63d2575ed99def39c
Iano-theDev <sinceianmike@gmail.com>
2022-09-09 05:27:42 +0300

    push

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+    this.width = w;
+    this.height = h;
+}
+    
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let tmp = this.width;
+    let widthHolder = this.width;
     this.width = this.height;
-    this.height = tmp;
+    this.height = widthHolder;
   }
+
   double () {
-    this.width *= 2;
-    this.height *= 2;
+    this.width = this.width*2;
+    this.height = this.height*2;
   }
 };
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -13,3 +13,10 @@ Write a clss Rectangle that defines a rectangle:
 ### 3. Rectangle #3
 Write a class Rectangle that defines a rectangle
 Create an instance method called print() that prints the rectangle using the character X.
+
+### 4. RE=ectagle #4
+write a class Rectangle thta defines a rectagle:
+* instance method print();
+* instance mehtod rotate() - exchanges the width and the height of the rectangle.
+* instance method double() - multiplies the width and the height of the rectangle by 2.
+
