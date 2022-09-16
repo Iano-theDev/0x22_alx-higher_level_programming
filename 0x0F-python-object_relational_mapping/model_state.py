2577ad865ff8b385129a66343c9da300525bd88b
Iano-theDev <sinceianmike@gmail.com>
2022-09-16 20:11:35 +0300

    Use sqlalchemy to create table for states as a class in python

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains class state and instance base
+"""
+
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Representation of a state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
