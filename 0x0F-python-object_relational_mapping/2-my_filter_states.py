8c74f5419747c18d6edae1737fde5459c959915e
Iano-theDev <sinceianmike@gmail.com>
2022-09-16 07:32:38 +0300

    revert to using 'format' string formatter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                   (argv[4],))
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC"
+                   .format(argv[4],))
     rows = cursor.fetchall()
     for row in rows:
        if row[1] == argv[4]:
