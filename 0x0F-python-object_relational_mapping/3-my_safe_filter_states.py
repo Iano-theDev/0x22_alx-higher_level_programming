6e20547df9cac69a028d2dfc7b13728ffdaf0f6e
Iano-theDev <sinceianmike@gmail.com>
2022-09-16 07:14:34 +0300

    update safe formatrt for task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,9 +7,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE {:s} ORDER BY id ASC", argv[4],)
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
+        id ASC", (argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
