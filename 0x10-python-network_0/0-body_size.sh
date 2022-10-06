868a49955ba46a2a5d610c82214fff3406c39e56
Iano-theDev <sinceianmike@gmail.com>
2022-10-06 11:24:05 +0300

    Change delimeter

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script takes in a url and sends a request to that url 
 # displays the size of the body of the response
 
-curl -sI "$1" | grep 'Content-Length' | cut -d ":" -f2
+curl -sI "$1" | grep 'Content-Length' | cut -d " " -f2
