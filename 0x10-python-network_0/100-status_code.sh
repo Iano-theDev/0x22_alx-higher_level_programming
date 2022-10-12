cd0fd7808c06854a892d3551fd0e52f7a1d7090d
Iano-theDev <sinceianmike@gmail.com>
2022-10-12 13:24:11 +0300

    remove body

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument and displays the only status code of the response
-curl -sI curl -s -o /dev/null -I -w "%{http_code}" "$1"
+curl -sI curl -s -o /dev/null -w "%{http_code}" "$1"
