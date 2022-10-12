5d2e4feff942d5220d00325d03e426de48aa97f6
Iano-theDev <sinceianmike@gmail.com>
2022-10-12 09:04:47 +0300

    add module documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""""""
+"""Contains function to find peak"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in alist of unsorted integers"""
+    """finds a peak in a list of unsorted integers"""
     li = list_of_integers
     l = len(li)
     if l == 0:
         return
     m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m -1]):
+    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
     if m != l - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
