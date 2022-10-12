3a6d7d6ca51409d4bef830d180a755ea53a80171
Iano-theDev <sinceianmike@gmail.com>
2022-10-12 13:49:06 +0300

    add server

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me causing the server to respond with a message in the body
-curl -sX -POST "You got me" "$1"
+curl -sLX PUT -d "You got me" 0.0.0.0:5000/catch_me
