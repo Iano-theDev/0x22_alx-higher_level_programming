3c980f733bd754cbd45a254b8fd0983ef1c71fc9
Iano-theDev <sinceianmike@gmail.com>
2022-10-12 07:57:53 +0300

    #

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a post request to the passed URL and displays the body of the response
-curl -o -sL  POST -H {"email=test@gmail.com&subject=I will always be here for PLD"} "$1"
+curl -o -sd {"email=test@gmail.com&subject=I will always be here for PLD"} "$1"
