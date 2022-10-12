e0962a78e4e31cae65d41b2b834e47e612d6e1b0
Iano-theDev <sinceianmike@gmail.com>
2022-10-12 13:22:31 +0300

    write out http status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument and displays the only status code of the response
-curl -sI "$1" | grep HTTP | cut -d " " -f 2
+curl -sI curl -s -o /dev/null -I -w "%{http_code}" "$1"
