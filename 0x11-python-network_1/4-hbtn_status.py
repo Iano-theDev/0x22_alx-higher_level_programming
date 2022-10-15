15ce8cfb5b2331fb9d707760ab7891c9b4332672
Iano-theDev <sinceianmike@gmail.com>
2022-10-15 14:54:03 +0300

    fetch url status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     import urllib.request as request
     import urllib.parse as parse
-    req = request.Request("https://alx-intranet.hbtn.io/status")
+    req = request.Request.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req)))
     print("\t- content: {}".format("content"))
