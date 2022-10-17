80b3c86f0fc119db9bf8cd903aff94c793800d37
Iano-theDev <sinceianmike@gmail.com>
2022-10-17 23:29:59 +0300

    Push staged commits

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me causing the server to respond with a message in the body
-curl -sLX PUT -H "You got me" 0.0.0.0:5000/catch_me
+curl -sX PUT -H "You got me" 0.0.0.0:5000/catch_me
