d87c016c7e0b69de7fbbe29a5ed2ef6a40fae8c4
Iano-theDev <sinceianmike@gmail.com>
2022-10-18 00:41:06 +0300

    Post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Post an email to a URL and displays the body of the response"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    email = {'email': argv[2]}
+    r = requests.post(argv[1], email)
+    text = r.text
+    print("{}".format(text))
