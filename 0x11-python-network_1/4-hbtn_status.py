6a91e9e2453c41e87234240f2118f1f3a181dc11
Iano-theDev <sinceianmike@gmail.com>
2022-10-17 23:40:05 +0300

    fix var name

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     import requests
     req = requests.get("https://alx-intranet.hbtn.io/status")
-    text = r.text
+    text = req.text
     print("Body response:")
     print("\t- type: {}".format(type(text)))
     print("\t- content: {}".format(text))
