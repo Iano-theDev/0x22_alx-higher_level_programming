b1852e39cd9f430a4849af38ed9a5c680ba5d694
Iano-theDev <sinceianmike@gmail.com>
2022-10-18 15:09:48 +0300

    get github commits using github api #pending

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Lists 10 commits (from the most recennt to the oldest)
+of the repository rails by the user ralis"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    # to be continued
